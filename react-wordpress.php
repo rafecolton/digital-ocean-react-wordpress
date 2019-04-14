9b2fd3b4d5774e828ae38701070b8f41400ebd44
Rafe Colton <rafael.colton@gmail.com>
2019-04-14 14:24:54 +0000

    Initial commit w/ plugin file and demo react app

---
+++ react-wordpress.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @link    https://github.com/<^>sammy/react-wordpress<^>
+ * @package Embedding React In Wordpress 
+ * @author  <^>Sammy <sammy@example.com><^>
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Embedding React In Wordpress
+ * Description:       Example plugin for embedding a React application in WordPress on Ubuntu 18.04
+ * Version:           0.1.0
+ * Author:            <^>Sammy<^>
+ */
+
+defined( 'ABSPATH' ) or die( 'Direct script access diallowed.' );
+
