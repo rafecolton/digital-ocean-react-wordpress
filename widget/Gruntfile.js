3c35932799a7f90fd00e08a561068ac6f02db5d0
Rafe Colton <rafael.colton@gmail.com>
2019-05-01 09:58:16 +0000

    Add watch stuff to gruntfile

--- widget/Gruntfile.js
+++ widget/Gruntfile.js
@@ -5,6 +5,17 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
+    watch: {
+      react: {
+        files: [
+          __dirname + 'package.json',
+          __dirname + '/src/**/*.js',
+          __dirname + '/public/index.html'
+        ],
+        tasks: ['shell:build']
+      },
+    },
+
     shell: {
       build: {
         command: 'GENERATE_SOURCEMAP=false NODE_PATH=$NODE_PATH:src yarn build --offline --production'
@@ -12,8 +23,9 @@ module.exports = function(grunt) {
     }
   });
 
+  grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-shell');
 
   grunt.registerTask('build', ['shell:build']);
-  grunt.registerTask('default', 'build');
+  grunt.registerTask('default', 'simple-watch');
 };
